6ae5ee48bee0a98b7402926eb1059601895662ac
Giovanni Aguirre <giovanni.fi05@gmail.com>
2021-02-19 20:56:00 -0600

    :directory: Path utilities

---
+++ futils/utils/path.py
@@ -0,0 +1,42 @@
+import os
+from pathlib import Path
+
+
+class InvalidPathError(Exception):
+    pass
+
+
+def is_dir(path: str):
+    """
+    Verifies that provided path exists as a directory
+    in os filesystem
+
+    :param path: Path to verify
+    """
+    return os.path.exists(path) and os.path.isdir(path)
+
+
+def path_files(path: str, extensions = []):
+    """
+    Creates a generator to iterate each file in provided
+    directory path
+
+    Yield values will be a string representing full path
+    to every file
+
+    :param path: Directory path
+    :param extensions: If value is provided, only files
+        with that extensions will be yield
+    """
+    if not is_dir(path):
+        raise InvalidPathError()
+
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            if not extensions:
+                yield os.path.join(path, file)
+
+            else:
+                file_ext = Path(file).suffix
+                if file_ext.lower() in extensions:
+                    yield os.path.join(path, file)
