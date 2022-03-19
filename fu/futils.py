6d2c83f52d9e38469b6fb001247f3af1ca701c79
Giovanni Aguirre <giovanni.fi05@gmail.com>
2022-03-18 20:22:50 -0600

    Enable tvshowfixnames as a command

--- fu/futils.py
+++ fu/futils.py
@@ -48,13 +48,14 @@ def moviefixname(src_dir: str = typer.Argument(
     """
     rename_movies(src_dir)
 
+@app.command()
 def tvshowfixnames(
     src_dir: str = typer.Argument(
         "./",
         help="Directory containing tv show files to rename"
     )
 ):
-    """Renames TV Show episodes files to make them scanners friendly
+    """Renames TV Show files to make them scanners friendly
     """
     rename_tvshow_files(src_dir)
 
