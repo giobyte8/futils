414d20827daaa0bf6c34fc66d65afcffa8b55515
Giovanni Aguirre <giovanni.fi05@gmail.com>
2022-08-30 20:52:51 -0500

    exif viewer for files list in text file

--- fu/futils.py
+++ fu/futils.py
@@ -119,5 +119,29 @@ def exif(
     inspect_datetime.inspect_dir(path, step)
 
 
+@app.command()
+def exif_pathsfile(
+    path: str = typer.Argument(
+        ...,
+        help='Path to file containing images paths'
+    ),
+    step: int = typer.Option(
+        1,
+        '--step',
+        '-s',
+        help='How many images/paths inspect at a time'
+    ),
+    display: bool = typer.Option(
+        False,
+        '--display',
+        '-d',
+        help='Indicates if each image should be displyed in system viewer'
+    )
+):
+    """Inspects exif datetime metadata for all file paths contained in a specific text file
+    """
+    inspect_datetime.inspect_from_file(path, step, display)
+
+
 if __name__ == "__main__":
     app()
