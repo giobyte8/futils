4925d36f9ba61dce2c8d994e28dcc458711e5c61
Giovanni Aguirre <giovanni.fi05@gmail.com>
2022-10-16 17:15:54 -0500

    Add exif inspect from pathsfile sub command

--- fu/exif/exif_app.py
+++ fu/exif/exif_app.py
@@ -21,3 +21,27 @@ def inspect(
     """Display exif date for all images found at given directory
     """
     inspect_datetime.inspect_dir(path, step)
+
+
+@app.command('inspect-from-pathsfile')
+def inspect_from_pathsfile(
+    path: str = typer.Argument(
+        ...,
+        help='Path to file containing images paths'
+    ),
+    step: int = typer.Option(
+        1,
+        '--step',
+        '-s',
+        help='How many images/paths inspect at a time'
+    ),
+    display: bool = typer.Option(
+        False,
+        '--display',
+        '-d',
+        help='Indicates if each image should be displyed in system viewer'
+    )
+):
+    """Inspects exif datetime metadata for all file paths contained in a specific text file
+    """
+    inspect_datetime.inspect_from_file(path, step, display)
